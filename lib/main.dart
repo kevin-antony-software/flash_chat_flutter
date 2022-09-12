eedbb6f580f5cd71e4ccb97cf603ff0fe0bc459c
kevin <kevin.antony.software@gmail.com>
2022-09-12 22:05:23 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flash_chat_flutter/screens/chat_screen.dart';
+import 'package:flash_chat_flutter/screens/login_screen.dart';
+import 'package:flash_chat_flutter/screens/registration_screen.dart';
+import 'package:flash_chat_flutter/screens/welcome_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(FlashChat());
+
+class FlashChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        textTheme: TextTheme(
+          bodyText1: TextStyle(color: Colors.black54),
+        ),
+      ),
+      initialRoute: 'welcome_screen',
+      routes: {
+        'welcome_screen': (context) => WelcomeScreen(),
+        'login_screen': (context) => LoginScreen(),
+        'registeration_screen': (context) => RegistrationScreen(),
+        'chat_scree': (context) => ChatScreen(),
+      },
+    );
+  }
+}
